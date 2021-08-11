da9babd001f6b3876df13c3902954306c8d562bb
medAzizRezgui <49117884+medAzizRezgui@users.noreply.github.com>
2021-08-11 16:06:27 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+      </div>
     </div>
   );
 }
